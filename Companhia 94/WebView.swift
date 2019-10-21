e8c03031e65bb0e1c1d2db9abeab632c48148f37
Hermes Alves Dias Souza <hermes@softagon.com.br>
2019-10-21 18:38:40 -0300

    Webiview simples do site

---
+++ Companhia 94/WebView.swift
@@ -0,0 +1,21 @@
+//
+//  WebView.swift
+//  Companhia 94
+//
+//  Created by Hermes Alves on 20/10/19.
+//  Copyright © 2019 Softagon Inovação e Gestão. All rights reserved.
+//
+
+import SwiftUI
+
+struct WebView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WebView_Previews: PreviewProvider {
+    static var previews: some View {
+        WebView()
+    }
+}
